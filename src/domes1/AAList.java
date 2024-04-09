8dbf3b355c9972afbb1b99b65b8119bd4208ae36
FenrirRhogar <103751641+FenrirRhogar@users.noreply.github.com>
2024-04-09 16:08:18 +0300

    aaa

--- src/domes1/AAList.java
+++ src/domes1/AAList.java
@@ -2,10 +2,21 @@ package domes1;
 
 public class AAList implements List {
 
+    int[][] list = new int[2][];
+
     public boolean insert(Element element) {
-        n = nextfree; //elexgos
-        nextfree = p[n] = p[nextfree] //elegxos gia an einai adeio
-        p[tail] = n;
-        p[n] = -1;
+        //n = nextfree; //elexgos
+        //nextfree = p[n] = p[nextfree] //elegxos gia an einai adeio
+        //p[tail] = n;
+        //p[n] = -1;
+        return false;
+    }
+
+    public boolean delete(int key) {
+        return false;
+    }
+
+    public Element search(int key) {
+        return null;
     }
 }
