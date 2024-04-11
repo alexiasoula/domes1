2243d0467c38c31f49c13ccf52ce81926dd41813
FenrirRhogar <103751641+FenrirRhogar@users.noreply.github.com>
2024-04-11 14:18:37 +0300

    pame ligo

--- src/domes1/main.java
+++ src/domes1/main.java
@@ -4,9 +4,15 @@ import java.util.Random;
 
 public class main {
 
+	public final static int MAX_SIZE = 50;
     public static void main(String[] args) {
-        // Δημιουργία της δομής δεδομένων DList
-        DList dList = new DList();
+        // Δημιουργία της δομών δεδομένων
+        DList dlist = new DList();
+        SDList sdlist = new SDList();
+        AAList aalist = new AAList(MAX_SIZE);
+        SAAList saalist = new SAAList(MAX_SIZE);
+        
+        Object[] obj = {dlist, sdlist, aalist, saalist};
 
         // Πίνακες για την αποθήκευση των μετρήσεων
         double[][] insertTimes = new double[10][6];
@@ -17,9 +23,9 @@ public class main {
         int[][] searchOperations = new int[10][6];
 
         // Επανάληψη για κάθε μέγεθος εισόδου N
-        int[] sizes = {30, 50, 100/*, 200, 500, 800, 1000, 5000, 10000, 100000*/};
-        for (int i = 0; i < sizes.length; i++) {
-            int size = sizes[i];
+        int[] N = {30, 50, 100, 200, 500, 800, 1000, 5000, 10000, 100000};
+        for (int i = 0; i < N.length; i++) {
+            int size = N[i];
             System.out.println("Size: " + size);
 
             // Προετοιμασία δεδομένων
@@ -28,27 +34,27 @@ public class main {
             for (int j = 0; j < size; j++) {
                 int key = random.nextInt(2 * size) + 1;
                 elements[j] = new MyElement(key);
-                dList.insert(elements[j]);
+                dlist.insert(elements[j]);
             }
 
             // Εκτέλεση μετρήσεων για την εισαγωγή
             System.out.println("Insertions:");
-            runMeasurements(dList, elements, size, insertTimes[i], insertOperations[i]);
+            runMeasurements(dlist, elements, size, insertTimes[i], insertOperations[i], 0);
 
             // Εκτέλεση μετρήσεων για τη διαγραφή
             System.out.println("Deletions:");
-            runMeasurements(dList, elements, size, deleteTimes[i], deleteOperations[i]);
+            runMeasurements(dlist, elements, size, deleteTimes[i], deleteOperations[i], 1);
 
             // Εκτέλεση μετρήσεων για την αναζήτηση
             System.out.println("Searches:");
-            runMeasurements(dList, elements, size, searchTimes[i], searchOperations[i]);
+            runMeasurements(dlist, elements, size, searchTimes[i], searchOperations[i], 2);
         }
 
         // Εμφάνιση αποτελεσμάτων
         // Εδώ μπορείτε να εμφανίσετε τα αποτελέσματα στον τρόπο που ταιριάζει στην προγραμματική σας αρχιτεκτονική
     }
 
-    private static void runMeasurements(DList dList, MyElement[] elements, int size, double[] times, int[] operations) {
+    private static void runMeasurements(DList dList, MyElement[] elements, int size, double[] times, int[] operations, int cs) {
         // Παράγετε τους τυχαίους αριθμούς
         Random random = new Random();
         int[] randomKeys = new int[1000];
@@ -57,39 +63,37 @@ public class main {
         }
 
         // Εκτέλεση μετρήσεων
-        for (int i = 0; i < 6; i++) {
-            long startTime = System.nanoTime();
-            int operationCount = 0;
-            for (int j = 0; j < 100; j++) {
-                switch (i) {
-                    case 0:
-                        for (int key : randomKeys) {
-                            MyElement element = new MyElement(key);
-                            dList.insert(element);
-                            operationCount++;
-                        }
-                        break;
-                    case 1:
-                        for (MyElement element : elements) {
-                            dList.delete(element.getKey());
-                            operationCount++;
-                        }
-                        break;
-                    case 2:
-                        for (int key : randomKeys) {
-                            dList.search(key);
-                            operationCount++;
-                        }
-                        break;
-                }
+        long startTime = System.nanoTime();
+        int operationCount = 0;
+        for (int j = 0; j < 100; j++) {
+            switch (cs) {
+                case 0:
+                    for (int key : randomKeys) {
+                        MyElement element = new MyElement(key);
+                        dList.insert(element);
+                        operationCount++;
+                    }
+                    break;
+                case 1:
+                    for (MyElement element : elements) {
+                        dList.delete(element.getKey());
+                        operationCount++;
+                    }
+                    break;
+                case 2:
+                    for (int key : randomKeys) {
+                        dList.search(key);
+                        operationCount++;
+                    }
+                    break;
             }
-            long endTime = System.nanoTime();
-
-            // Καταγραφή των μέσων τιμών του χρόνου και των πράξεων
-            double averageTime = (endTime - startTime) / 1e6 / 100; // μέσος όρος σε ms
-            times[i] = averageTime;
-            operations[i] = operationCount / 100;
-            System.out.println("Operation: " + (char)('A' + i) + ", Time: " + averageTime + " ms, Operations: " + operations[i]);
         }
+        long endTime = System.nanoTime();
+
+        // Καταγραφή των μέσων τιμών του χρόνου και των πράξεων
+        double averageTime = (endTime - startTime) / 1e6 / 100; // μέσος όρος σε ms
+        times[cs] = averageTime;
+        operations[cs] = operationCount / 100;
+        System.out.println("Operation: " + (char)('A' + cs) + ", Time: " + averageTime + " ms, Operations: " + operations[cs]);
     }
 }
